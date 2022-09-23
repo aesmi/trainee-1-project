eaa09ade6cd818b9b4ecc3e693d36a3ae2e51d98
Rolguard <76728853+Rolguard@users.noreply.github.com>
2022-09-23 19:06:11 +1000

    Added some css and InputField component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
+import "./css/mainPage.css";
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+    console.log(input)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>insertProjectName</h1>
+      <h2>Let Me Google That For You...</h2>
+      <InputField onInput={handleChange} />
     </div>
   );
 }
---
+++ src/components/InputField.js
@@ -0,0 +1,8 @@
+const InputField = ({ onInput }) => {
+
+    return (
+        <input type="text" className="search-field" name="search-result" onChange={(e) => onInput(e)} />
+    );
+}
+
+export default InputField
