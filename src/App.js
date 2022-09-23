7ac008a59488aa4d87f0f431f34ad6cee373047f
Jane <janedqwang@gmail.com>
2022-09-23 22:11:28 +1000

    do not even worry about the css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import InputField from './components/InputField';
 import "./css/mainPage.css";
 import { useState } from 'react';
+import Logo from "./assets/duckduckgo-logo.png"
 
 function App() {
   const [input, setInput] = useState("");
@@ -15,9 +14,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>insertProjectName</h1>
-      <h2>Let Me Google That For You...</h2>
-      <InputField onInput={handleChange} />
+      <div className="this-is-everything">
+        <img className="logo" src={Logo}/>
+        <p>(totally original idea)</p>
+        <h1>?!?</h1>
+        <h2>Let Me <span style={{textDecoration: "line-through"}}>Google</span> DuckDuckGo That For You...</h2>
+        <form className="search-bar">
+          <input id="search-input" type="text" autocomplete="off" autocapitalize="off" placeholder="Let me do it for you :)"/>
+          {/* search form clear button needs to remove the input field */}
+          <input id="search-form-clear" type="button" value="X"/>
+        </form>
+        {/* <InputField onInput={handleChange} /> */}
+      </div>
     </div>
   );
 }
