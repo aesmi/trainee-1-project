8a82b4c5ab2f60b5ecdf17bfe32cab76ae39c095
joshi-hahahaha <joshi.ha7@gmail.com>
2022-09-25 19:17:18 +1000

    Changes to logo and basic layout to the website.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import axios from 'axios'
 import InputField from "./components/InputField";
 import WeatherPopupBox from "./components/WeatherPopupBox";
 import "./css/mainPage.css";
+import "./css/weather.css"
 import "./css/weather-search.css";
 import "./css/duckduck-search.css";
-import Logo from "./assets/duckduckgo-logo.png";
+import Logo from "./assets/duckduckgo.png";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  // const url =`https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=483fbfa21ef33128ac6d64c113da5d67`
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=483fbfa21ef33128ac6d64c113da5d67`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        //console.log(repsonse.data)
+      })
+    }
+  }
 
   const [input, setInput] = useState("");
+  ///////////////////////////////////// <-- to delete
   const [loc, setLoc] = useState({});
+  /////////////////////////////////////
 
   const handleChange = (e) => {
     setInput(e.target.value);
     console.log(input);
   };
 
+  ///////////////////////////////////// <-- to delete
   function success(loc) {
     const crd = loc.coords;
 
@@ -37,16 +52,31 @@ function App() {
   function handleLocation() {
     navigator.geolocation.getCurrentPosition(success, error);
   }
+  ////////////////////////////////////////////////////////////
 
   return (
     <div className="App">
-        <div className="weather">
-          <div className="location">
-            <p>Sydney</p>
-            <WeatherPopupBox>hello</WeatherPopupBox>
+      <div className="weather">
+        <div className="location">
+          <WeatherPopupBox>hello</WeatherPopupBox>
           <button onClick={handleLocation}>console.logging location</button> {/*this will be removed soon*/}
-          </div>
         </div>
+        <div className="temp">
+          <h1>20 deg</h1>
+        </div>
+        <div className="desc">
+          <p>clear</p>
+        </div>
+        <div className="feels">
+          <p>21</p>
+        </div>
+        <div className="humidity">
+          <p>20%</p>
+        </div>
+        <div className="wind">
+          <p>12kmh</p>
+        </div>
+      </div>
 
       <div className="duckduck">
         <img className="logo" src={Logo} />
@@ -68,6 +98,10 @@ function App() {
           <input id="search-form-clear" type="button" value="X" />
         </form>
         {/* <InputField onInput={handleChange} /> */}
+      </div>
+
+      <div className="">
+
       </div>
     </div>
   );
