67f41f6a1542aff4c353add210135a316be93578
Rolguard <76728853+Rolguard@users.noreply.github.com>
2022-09-26 15:28:08 +1000

    Added routes and parsing of url parameters for SearchPage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import axios from 'axios'
-import WeatherPopupBox from "./components/WeatherPopupBox";
 import "./css/mainPage.css";
 import "./css/weather.css"
 import "./css/weather-search.css";
 import "./css/duckduck-search.css";
-import Logo from "./assets/duckduckgo.png";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import IndexPage from "./components/IndexPage";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-
-  // const url =`https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=483fbfa21ef33128ac6d64c113da5d67`
-  // e.g. DDG url for 'most popular sport in the world' https://duckduckgo.com/?q=most+popular+sport+in+the+world&va=j&t=hc&ia=web
+  const [input, setInput] = useState("");
   const siteUrl = "https://duckduckgo.com/"
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=483fbfa21ef33128ac6d64c113da5d67`
 
@@ -25,17 +24,16 @@ function App() {
       setLocation("")
     }
   }
-
-  const [input, setInput] = useState("");
   ///////////////////////////////////// <-- to delete
   const [loc, setLoc] = useState({});
   /////////////////////////////////////
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const searchUrl = input.replace(/ /g, "+");
-    const resUrl = siteUrl + "?q=" + searchUrl + "&va=j&t=hc&ia=web";
-    const win = window.open(resUrl, "_blank");
+    console.log(input)
+    let searchUrl = input.replace(/ /g, "+");
+    let resUrl = siteUrl + "?q=" + searchUrl + "&va=j&t=hc&ia=web";
+    let win = window.open(resUrl, "_blank");
     win.focus()
   }
 
@@ -60,69 +58,31 @@ function App() {
   }
   ////////////////////////////////////////////////////////////
 
+
   return (
-    <div className="App">
-      <div className="weather">
-        <div className="search">
-          <input
-            value={location}
-            onChange={event => setLocation(event.target.value)}
-            onKeyPress={searchLocation}
-            placeholder="Enter Location"
-            type="text"
-          />
-        </div>
-        <div className="location">
-          <p>{data.name}</p>
-          <WeatherPopupBox>hello</WeatherPopupBox>
-          <button onClick={handleLocation}>console.logging location</button> {/*this will be removed soon*/}
-        </div>
-        <div className="temp">
-          {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
-        </div>
-        <div className="desc">
-          {data.weather ? <p>{data.weather[0].main}</p> : null}
-        </div>
-        <div className="feels">
-          {data.main ? <p>feels like {data.main.feels_like.toFixed()}°C</p> : null}
-        </div>
-        <div className="humidity">
-          {data.main ? <p>humidity {data.main.humidity}%</p> : null}
-        </div>
-        <div className="wind">
-          {data.wind ? <p>wind {data.wind.speed.toFixed()} mph</p> : null}
-        </div>
-      </div>
-
-      <div className="duckduck">
-        <img className="logo" src={Logo} alt="DuckDuckGo Logo" />
-        <p>(totally original idea)</p>
-        <h1>?!?</h1>
-        <h2>
-          Let Me <span style={{ textDecoration: "line-through" }}>Google</span>{" "}
-          DuckDuckGo That For You...
-        </h2>
-        <form className="search-bar" role="search" onSubmit={handleSubmit}>
-          <input
-            id="search-input"
-            type="text"
-            autocomplete="off"
-            autocapitalize="off"
-            placeholder="Let me do it for you :)"
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-          />
-          {/* search form clear button needs to remove the input field  */}
-          {/* Typing any input can reveal a search icon which the user can click instead of having to press enter */}
-          <input id="search-form-clear" type="button" value="X" />
-        </form>
-      </div>
-
-      <div className="">
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/search" element={<SearchPage />}>
+            {/* Play animation searching in search bar {`/?q=${input}`*/}
+          </Route>
+          {/* 
+          Put all content previously in main page into IndexPage component
+          Might need to use Context API if we nest more components into IndexPage which require access to global state
+          e.g. location and setLocation, data and setData */}
+          <Route path="/" element={<IndexPage
+            data={data}
+            location={location}
+            setLocation={setLocation}
+            handleLocation={handleLocation}
+            searchLocation={searchLocation}
+            handleSubmit={handleSubmit}
+            setInput={setInput} />}>
+          </Route>
+        </Routes>
 
       </div>
-    </div>
+    </Router>
   );
 }
 
