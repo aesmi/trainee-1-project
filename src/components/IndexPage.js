0bfe9897d5d7404af9933bc34f9439d613dea5c2
Rolguard <76728853+Rolguard@users.noreply.github.com>
2022-09-27 16:06:15 +1000

    Added typing animation in search page and separated JSX for search box into SearchBox component

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,11 +1,8 @@
 import WeatherPopupBox from "./WeatherPopupBox";
-import Logo from "../assets/duckduckgo.png";
-import { useSearchParams } from 'react-router-dom';
+import SearchBox from "./SearchBox";
+
 
 const IndexPage = ({ data, location, setLocation, handleLocation, searchLocation, handleSubmit, setInput }) => {
-    const [searchParams] = useSearchParams();
-    console.log(searchParams);
-
     return (
         <>
             <div className="weather">
@@ -39,44 +36,7 @@ const IndexPage = ({ data, location, setLocation, handleLocation, searchLocation
                     {data.wind ? <p>wind {data.wind.speed.toFixed()} mph</p> : null}
                 </div>
             </div>
-
-            <div className="duckduck">
-                <img className="logo" src={Logo} alt="DuckDuckGo Logo" />
-                <p>(totally original idea)</p>
-                <h1>?!?</h1>
-                <h2>
-                    Let Me <span style={{ textDecoration: "line-through" }}>Google</span>{" "}
-                    DuckDuckGo That For You...
-                </h2>
-                <form className="search-bar" role="search" onSubmit={handleSubmit}>
-                    {/* TODO: Instead of searching the result directly on duckduckgo, it should generate a URL
-                    e.g. localhost:3000/search/?q=most+popular+sport which when the user types into their browser
-
-                    Opens the SearchPage which will run an animation
-
-                    Technically the functionality could of just been done conditionally in the IndexPage
-                    but I think it's cleaner to divide into different pages + provides option to pick and choose
-                    which elements I want to use/exclude from the IndexPage without added complexity of
-                    conditionally rendering each component based on url parameters*/}
-                    <input
-                        id="search-input"
-                        type="text"
-                        autoComplete="off"
-                        autoCapitalize="off"
-                        placeholder="Let me do it for you :)"
-                        onChange={(e) => {
-                            setInput(e.target.value);
-                        }}
-                    />
-                    {/* search form clear button needs to remove the input field  */}
-                    {/* Typing any input can reveal a search icon which the user can click instead of having to press enter */}
-                    <input id="search-form-clear" type="button" value="X" />
-                </form>
-            </div>
-
-            <div className="">
-
-            </div>
+            <SearchBox handleSubmit={handleSubmit} setInput={setInput} />
         </>
     )
 }
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,23 +1,62 @@
 import { useSearchParams } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
+import SearchBox from './SearchBox';
+import '../css/searchPage.css'
+
 const SearchPage = () => {
     const [searchParams] = useSearchParams();
     /*
+        Use react-type-animation
         Need to detect the ?q= then at least 1 character after equals that is not whitespace
-        Need to account for what happens if params provided is only whitespace or 
+       TODO: Need to account for what happens if params provided is only whitespace or 
         If no params are provided at all e.g. localhost:3000/search
         --> Redirect to main page?
 
         Else if params are provided that is not whitespace --> Play animation
+
+        Message box at bottom with instructions,
+        Message box says Step 1: type your search result --> fade into loading animation --> into next step
+        Animation with keyboard sound effects 
     */
 
     console.log(Object.fromEntries([...searchParams]));
     let paramObj = Object.fromEntries([...searchParams]);
+    // Create a time variable which consists of the total time to run the TypeAnimation
+
+    // queryArr uses random delay in ms between words to simulate more human typing
+    // E.g. [most, popular, sport] --> [2000, most, 1200, popular, 1500, sport]
+    const queryArr = paramObj.q.split(" ")
+    console.log(`queryArr contains: ${queryArr}`)
+    console.log(`queryArr length is: ${queryArr.length}`)
+    // const seqArr = queryArr.map((val, idx) => {
+    //     console.log(`idx is ${idx}`)
+    //     queryArr.splice(idx, 0, Math.floor(Math.random() * 2000) + 1000)
+    //     return
+    // })
+
+
+    // for (let i = 0; i < queryArr.length; i + 2) {
+    //     queryArr.splice(i, 0, Math.floor(Math.random() * 2000) + 1000)
+    // }
+    // console.log(`queryArr contains: ${queryArr}`)
+
 
     return (
         <>
-            {/* {console.log(encodeURIComponent("?q="))} */}
-            <div>
-                <p>{`My new page with url params: ${searchParams}`}</p>
+            <div className="search-container">
+                {/* {console.log(encodeURIComponent("?q="))} */}
+                <SearchBox
+                    handleSubmit={(e) => e.preventDefault()}
+                    setInput={() => { }}
+                    typeAnimation={
+                        <TypeAnimation sequence={[
+                            2000,
+                            paramObj.q
+                        ]}
+                            className="type-animation"
+                            cursor={true}
+                            speed={20}
+                        />} />
             </div>
         </>
     )
